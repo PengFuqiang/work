02f33927ebae8b123cb6b4d0da9320cf5646fb99
PengFuqiang <531326574@qq.com>
2018-12-05 16:17:56 +0800

    Create worker_app_room_status.php

---
+++ worker_app_room_status.php
@@ -0,0 +1,19 @@
+<?php
+$table = 't_room_status';
+include '../conn.php';
+
+try{
+        $sth = $dbh->prepare("select * from $table order by timestamp");
+        $sth->execute();
+        $items = array();
+        while($row = $sth->fetch(PDO::FETCH_ASSOC)){
+                array_push($items, $row);
+        }
+        $result = $items;
+}catch(PDOEXception $e){
+        $result = array();
+        $result['isError'] = $e->getMessage();
+        echo json_encode($result);
+        exit;
+}
+echo json_encode($result);
